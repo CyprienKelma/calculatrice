39f237ca5e0606b8396d7affb9878573778f7bed
CyprienKelma <90218833+CyprienKelma@users.noreply.github.com>
2023-05-03 10:16:51 +0200

    Update success.module.ts

--- calculator-project/src/success/success.module.ts
+++ calculator-project/src/success/success.module.ts
@@ -1,4 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ValideController } from './valide.controller';
+import { ValideService } from './valide.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Valide } from './valide.entity';
 
-@Module({})
-export class SuccessModule {}
+@Module({
+  imports:[
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'Calculatrice',
+      entities: ["dist/**/*.entity{.ts,.js}"],
+      synchronize: false,
+    }), TypeOrmModule.forFeature([Valide]),
+  ],
+  controllers: [ValideController],
+  providers: [ValideService]
+})
+export class ValideModule {}
+  
