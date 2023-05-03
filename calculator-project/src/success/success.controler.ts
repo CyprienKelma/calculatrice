a548b1bea267ff2f06dee61bd9764c31c594862a
CyprienKelma <90218833+CyprienKelma@users.noreply.github.com>
2023-05-03 10:19:54 +0200

    Create success.controler.ts

---
+++ calculator-project/src/success/success.controler.ts
@@ -0,0 +1,22 @@
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { ValideService } from './valide.service';
+import { Valide } from './valide.entity';
+
+@Controller('valide')
+export class ValideController {
+
+    constructor(private readonly valideService: ValideService) {}
+
+    @Post()
+    createValid(
+        @Body('timeTakenMs') timeTakenMs: number,
+    ): Promise<Valide> {
+        return this.valideService.createValid(timeTakenMs);
+    }
+
+    @Get()
+    getMoyenne(): Promise<number> {
+        return this.valideService.getMoyenne();
+    }
+
+}
